a23a23df4d8dbaf642a8e71d43d24bd17c10ac7d
Dybal-Ilia <dybal.156@gmail.com>
2025-08-20 11:12:48 +0300

    DS/1.1 Slightly changed Plotter behaviour

--- src/plotter/plotter.py
+++ src/plotter/plotter.py
@@ -7,36 +7,34 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
 class Plotter:
-    def __init__(self, df:pd.DataFrame):
-        self.df = df
 
     
-    def plot_distributions(self):
-        for col in self.df.select_dtypes(include=[np.number]):
+    def plot_distributions(self, df):
+        for col in df.select_dtypes(include=[np.number]):
             plt.figure()
-            plt.hist(self.df[col], bins = 100)
+            plt.hist(df[col], bins = 100)
             plt.title(f"{col} distribution")
             plt.xlabel(f"{col}")
             plt.ylabel("Amount")
             plt.show() 
 
     
-    def plot_linear_corr(self):
-        corr = self.df.select_dtypes(include=[np.number]).corr()
+    def plot_linear_corr(self, df):
+        corr = df.select_dtypes(include=[np.number]).corr()
         plt.figure()
         plt.title("Correlation matrix")
         sns.heatmap(corr, annot=True, fmt=".3f")
         plt.show()
 
 
-    def plot_timeseries(self):
+    def plot_timeseries(self, df):
         plt.figure(figsize=(20, 10))
-        sns.lineplot(data=self.df, x="date", y="item_cnt_day")
+        sns.lineplot(data=df, x="date", y="item_cnt_day")
         plt.show()
 
 
-    def plot_timeseries_decomposed(self):
-        ts = self.df.groupby("date")["item_cnt_day"].sum()
+    def plot_timeseries_decomposed(self, df):
+        ts = df.groupby("date")["item_cnt_day"].sum()
         decomposed = seasonal_decompose(ts, model="additive", period=30)
         fig, axes =plt.subplots(3, 1, figsize = (15, 10), sharex=True)
         decomposed.trend.plot(ax=axes[0], title="trend")
