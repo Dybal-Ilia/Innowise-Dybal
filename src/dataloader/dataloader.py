ee627ee446eb59f953cc9a15f4d63c59d09ba51c
Dybal-Ilia <dybal.156@gmail.com>
2025-08-20 11:11:07 +0300

    DS/1.1 Added merge_data function

--- src/dataloader/dataloader.py
+++ src/dataloader/dataloader.py
@@ -2,13 +2,24 @@ import opendatasets as od
 from collections import defaultdict
 import os
 import pandas as pd
+from typing import Dict
 
 
 def donwload_data(url):
     od.download(url)
     dataset_name = url.split("/")[-1]
     dataframes = defaultdict()
+
     for dirname, _, filenames in os.walk(f'{os.getcwd()}/{dataset_name}'):
         for filename in filenames:
-            dataframes[filename] = pd.read_csv(f"{dirname}/{filename}")
+            dataframes[filename.split(".")[0]] = pd.read_csv(f"{dirname}/{filename}")
+
     return dataframes
+
+def merge_data(dataframes: Dict[str, pd.DataFrame]):
+        df = dataframes["sales_train"].merge(dataframes["items"], on='item_id', how='left').merge(dataframes["item_categories"], on='item_category_id', how='left').merge(dataframes["shops"], on='shop_id', how='left')
+        return df
+
+
+
+
