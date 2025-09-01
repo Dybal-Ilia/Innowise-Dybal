cd6174850e0f1c313ddbff35c75554bacaceb195
Dybal-Ilia <dybal.156@gmail.com>
2025-09-01 15:21:42 +0300

    DS/1.1 style: added typehints

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -4,7 +4,7 @@ import sys
 import logging
 from decorators.exec_time import exec_time
 import datetime as dt
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from collections import defaultdict
 import opendatasets as od
 import numpy as np
@@ -17,35 +17,33 @@ logger = logging.getLogger(__name__)
 
 class ETL:
     
-
-
-    def __init__(self, map_pipes, filter_pipes):
+    def __init__(self, map_pipes: Dict[str, List], filter_pipes: Dict[str, List]):
         self.map_pipes = map_pipes
         self.filter_pipes = filter_pipes
         
 
-    def apply_map(self, tables):
+    def apply_map(self, tables: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
         return self._apply_pipes(tables, self.map_pipes)
 
 
-    def apply_filter(self, tables):
+    def apply_filter(self, tables: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
         return self._apply_pipes(tables, self.filter_pipes)
     
 
-    def _apply_pipes(self, tables, pipes):
+    def _apply_pipes(self, tables: Dict[str, pd.DataFrame], pipes) -> Dict[str, pd.DataFrame]:
         for table in tables:
             if table in pipes:
                 pipe = pipes[table]
                 tables[table] = self._apply_pipes(tables[table], pipe)
         return tables
     
-    def _apply_pipe(self, table, pipe):
+    def _apply_pipe(self, tables: Dict[str, pd.DataFrame], pipe) -> pd.DataFrame:
         for func in pipe:
             table = func(table)
         return table
     
 
-    def apply_merge(self, tables):
+    def apply_merge(self, tables: Dict[str, pd.DataFrame]) -> Tuple[pd.DataFrame, pd.DataFrame]:
         merged_tables_train = (
             tables['sales_train']
             .merge(tables['items'], on = 'item_id', how = 'left')
@@ -64,7 +62,7 @@ class ETL:
 
 
 
-    def extract(self, url:str):
+    def extract(self, url:str) -> Dict[str, pd.DataFrame]:
 
         """Downloads data from the provided url"""
 
